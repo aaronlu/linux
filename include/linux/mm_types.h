d98ce4d415f2cb6d1d0232cf994e94e206594196
Aaron Lu <aaron.lu@intel.com>
2018-02-28 16:46:26 +0800

    mm/__free_one_page: skip merge for order-0 page unless compaction failed
    Running will-it-scale/page_fault1 process mode workload on a 2 sockets
    Intel Skylake server showed severe lock contention of zone->lock, as
    high as about 80%(42% on allocation path and 35% on free path) CPU
    cycles are burnt spinning. With perf, the most time consuming part inside
    that lock on free path is cache missing on page structures, mostly on
    the to-be-freed page's buddy due to merging.
    
    One way to avoid this overhead is not do any merging at all for order-0
    pages. With this approach, the lock contention for zone->lock on free
    path dropped to 1.1% but allocation side still has as high as 42% lock
    contention. In the meantime, the dropped lock contention on free side
    doesn't translate to performance increase, instead, it's consumed by
    increased lock contention of the per node lru_lock(rose from 5% to 37%)
    and the final performance slightly dropped about 1%.
    
    Though performance dropped a little, it almost eliminated zone lock
    contention on free path and it is the foundation for the next patch
    that eliminates zone lock contention for allocation path.
    
    Suggested-by: Dave Hansen <dave.hansen@intel.com>
    Signed-off-by: Aaron Lu <aaron.lu@intel.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -93,6 +93,7 @@ struct page {
 		pgoff_t index;		/* Our offset within mapping. */
 		void *freelist;		/* sl[aou]b first free object */
 		/* page_deferred_list().prev	-- second tail page */
+		bool buddy_merge_skipped; /* skipped merging when added to buddy */
 	};
 
 	union {
